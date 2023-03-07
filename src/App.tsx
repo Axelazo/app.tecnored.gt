7755f3f4c35418e9dffd14667745daff4b5a0daa
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-06 22:17:05 -0600

    Update App.tsx
    Added user (AuthProvider) for conditional rendering based on auth state // TODO: Refactor to take user into consideration roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Login from "./pages/Login";
+import Clients from "./pages/Clients";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/router/PrivateRoute";
+
+import { useAuth } from "./providers/AuthProvider";
 
 function App() {
+  const { user } = useAuth();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {user ? (
+          <Route element={<Sidebar />}>
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/clients" element={<Clients />} />
+              <Route path="*" element={<Navigate to="/dashboard" />} />
+            </Route>
+          </Route>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 }
 
