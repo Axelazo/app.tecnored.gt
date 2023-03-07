6e49233f2e5245e945170d882d8b80ff1978b4b9
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-06 22:13:49 -0600

    Create AuthProvider.tsx
    Added AuthProvider for entire app

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,74 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+export interface Role {
+  id: number;
+  roleName: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface User {
+  id: number;
+  firstNames: string;
+  lastNames: string;
+  email: string;
+  roles: Role[];
+  token: string;
+}
+
+interface AuthContextValue {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>> | null;
+  login: (user: User) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextValue>({
+  user: null,
+  setUser: null,
+  login: () => {},
+  logout: () => {},
+});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  useEffect(() => {
+    console.log(user);
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+    console.log(user);
+  }, [user]);
+
+  const login = (user: User) => {
+    setUser(user);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
