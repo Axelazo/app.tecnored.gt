8eff24c3a2d9d8bf23e821d8f85446748639ad1d
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-09 14:51:39 -0600

    Create DualSideDivider.tsx

---
+++ src/components/DualSideDivider.tsx
@@ -0,0 +1,26 @@
+import { Flex, Divider, Text } from "@chakra-ui/react";
+
+interface DualSideDividerProps {
+  text: string;
+  width: Number;
+}
+
+function DualSideDivider({ text, width }: DualSideDividerProps) {
+  return (
+    <Flex align="center">
+      <Divider />
+      <Text
+        padding="2"
+        wordBreak={"keep-all"}
+        minW={`${width}rem`}
+        textAlign={"center"}
+        fontWeight={"light"}
+      >
+        {text}
+      </Text>
+      <Divider />
+    </Flex>
+  );
+}
+
+export default DualSideDivider;
