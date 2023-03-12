72c7511dfc5713a2d16abad04af07ea4e304f3b1
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-12 16:02:29 -0600

    Update Login.tsx
    Refactored using react-hook-form
    Added User Login resolver
    Removed bloat
    Removed unused imports

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,8 @@
+import ApiClient from "../api/api";
+
 import {
   Flex,
   Box,
-  FormControl,
-  FormLabel,
-  Input,
   Checkbox,
   Stack,
   Link,
@@ -11,100 +10,82 @@ import {
   Heading,
   Text,
   useColorModeValue,
-  FormErrorMessage,
-  InputGroup,
-  InputRightElement,
   useToast,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { FaEyeSlash, FaEye } from "react-icons/fa";
-import ApiClient from "../api/api";
 import { Logo } from "../components/Logo";
-import { useAuth, User } from "../providers/AuthProvider";
+import { useAuth } from "../providers/AuthProvider";
+import {
+  User,
+  UserLoginAPIResponse,
+  UserLoginFormValues,
+} from "interfaces/User";
+import { Resolver, useForm } from "react-hook-form";
+import PasswordInput from "components/login/PasswordInput";
+import ValidatableInput from "components/login/ValidatableInput";
 
 const api = new ApiClient();
 
-interface LoginErrors {
-  email: string;
-  password: string;
-}
-
-interface LoginApiResponse {
-  status: number;
-  data: User;
-}
+const resolver: Resolver<UserLoginFormValues> = async (
+  values: UserLoginFormValues
+) => {
+  return {
+    values: values.email && values.password ? values : {},
+    errors: {
+      ...(!values.email && {
+        email: {
+          type: "required",
+          message: "El correo electrónico es requerido!",
+        },
+      }),
+      ...(values.email &&
+        !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email) && {
+          email: {
+            type: "invalid",
+            message: "El correo electrónico debe ser válido!",
+          },
+        }),
+      ...(!values.password && {
+        password: {
+          type: "required",
+          message: "La contraseña es requerida!",
+        },
+      }),
+    },
+  };
+};
 
 export default function Login() {
+  const { setUser } = useAuth();
+  const navigate = useNavigate();
   const toast = useToast();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isEmailError, setIsEmailError] = useState(false);
-  const [isPasswordError, setIsPasswordError] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const [errors, setErrors] = useState<LoginErrors>({
-    email: "",
-    password: "",
+
+  const { register, handleSubmit, formState, clearErrors } =
+    useForm<UserLoginFormValues>({
+      resolver,
+    });
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await login(data);
+    } catch (error) {
+      console.error(error);
+    }
   });
 
-  const { setUser } = useAuth();
-
-  const navigate = useNavigate();
-
-  const resetLoginWarnings = () => {
-    setTimeout(() => {
-      setIsLoading(false);
-
-      setIsEmailError(false);
-      setIsPasswordError(false);
-    }, 3000);
-  };
-
-  const handleLogin = () => {
-    setIsEmailError(false);
-    setIsPasswordError(false);
-    setIsLoading(true);
-
-    setTimeout(() => {
-      let updatedErrors = {
-        email: "",
-        password: "",
-      };
-
-      if (!email) {
-        updatedErrors.email = "Ingrese su correo electrónico";
-        setErrors((errors) => ({
-          ...errors,
-          ...updatedErrors,
-        }));
-        setIsEmailError(true);
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-        updatedErrors.email = "El correo electrónico no es valido";
-        setErrors((errors) => ({
-          ...errors,
-          ...updatedErrors,
-        }));
-        setIsEmailError(true);
-      }
-
-      if (!password) {
-        setIsPasswordError(true);
-      }
-
-      if (!password || !email) {
-        setIsEmailError(true);
-        setIsPasswordError(true);
-      } else {
+  const login = async ({ email, password }: UserLoginFormValues) => {
+    return new Promise<User>((resolve, reject) => {
+      const timeout = Math.floor(Math.random() * 2000) + 1000; // Random wait time between 1-3 seconds
+      setTimeout(() => {
         api
-          .post<LoginApiResponse>("/signin", {
-            email: email,
-            password: password,
+          .post<UserLoginAPIResponse>("/signin", {
+            email,
+            password,
           })
           .then((response) => {
-            setIsLoading(true);
-            const userResponse = response.data;
-            setUser?.(userResponse);
+            setUser?.(response.data);
+            navigate({ pathname: "/dashboard" });
             toast({
               description: `Bienvenido ${
                 response.data.firstNames.split(" ")[0]
@@ -114,23 +95,20 @@ export default function Login() {
               isClosable: true,
             });
             navigate({ pathname: "/dashboard" });
+            resolve(response.data);
           })
-          .catch((error) => {
-            setIsLoading(false);
+          .catch((reason) => {
             toast({
               description: "Nombre de usuario o contraseña incorrectos.",
               status: "error",
               duration: 3000,
               isClosable: true,
             });
+            reject(reason);
           });
-      }
-
-      resetLoginWarnings();
-    }, Math.random() * 1000);
+      }, timeout);
+    });
   };
-
-  const handleShowClick = () => setShowPassword(!showPassword);
 
   return (
     <Flex
@@ -156,55 +134,41 @@ export default function Login() {
           p={8}
         >
           <Stack spacing={4}>
-            <FormControl id="email" isInvalid={isEmailError}>
-              <FormLabel>Correo electrónico</FormLabel>
-              <Input type="email" onChange={(e) => setEmail(e.target.value)} />
-              {!isEmailError ? (
-                ""
-              ) : (
-                <FormErrorMessage>{errors.email}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl id="password" isInvalid={isPasswordError}>
-              <FormLabel>Contraseña</FormLabel>
-              <InputGroup>
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <InputRightElement>
-                  <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                    {showPassword ? <FaEyeSlash /> : <FaEye />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-              {!isPasswordError ? (
-                ""
-              ) : (
-                <FormErrorMessage>Ingrese su contraseña.</FormErrorMessage>
-              )}
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: "column", sm: "row" }}
-                align={"start"}
-                justify={"space-between"}
-              >
-                <Checkbox>Recordarme</Checkbox>
-                <Link color={"blue.400"}>Olvidaste tu contraseña?</Link>
+            <form onSubmit={onSubmit} noValidate={true}>
+              <ValidatableInput
+                label="Correo electrónico"
+                id="email"
+                register={register("email")}
+                type={"email"}
+                error={formState.errors.email}
+              />
+              <PasswordInput
+                label="Contraseña"
+                register={register("password")}
+                error={formState.errors.password}
+              />
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  <Checkbox>Recordarme</Checkbox>
+                  <Link color={"blue.400"}>Olvidaste tu contraseña?</Link>
+                </Stack>
+                <Button
+                  isLoading={formState.isSubmitting}
+                  type="submit"
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  Iniciar Sesión
+                </Button>
               </Stack>
-              <Button
-                isLoading={isLoading}
-                onClick={handleLogin}
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-              >
-                Iniciar Sesión
-              </Button>
-            </Stack>
+            </form>
           </Stack>
         </Box>
       </Stack>
