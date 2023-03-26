932786ac4f802f9c632a7419099360e40dfc7586
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-26 12:25:22 -0600

    Update App.tsx
    Added conditional rendering based on user roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
   Navigate,
+  Route,
+  Routes,
 } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Login from "./pages/Login";
-import Clients from "./pages/Clients";
-import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/router/PrivateRoute";
+import Dashboard from "pages/Dashboard";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import Sidebar from "./components/Sidebar";
+import Clients from "pages/clients/Clients";
+import ClientList from "components/clients/ClientsList";
+import CreateClient from "components/clients/CreateClient";
 
-import { useAuth } from "./providers/AuthProvider";
+import { useAuth } from "providers/AuthProvider";
+import ViewClient from "components/clients/ViewClient";
+import UpdateClient from "components/clients/UpdateClient";
+import Dummy from "pages/Dummy";
 
 function App() {
   const { user } = useAuth();
+
+  // Define an array of roles that can access specific routes
+  //const allRoles = ["admin", "operator", "technician", "worker", "user"];
+  const userRoutes = ["admin", "user"];
+  const workerRoutes = ["admin", "worker"];
+  const operatorRoutes = ["admin", "operator"];
+  const adminRoutes = ["admin"];
+
+  // Filter the roles of the user to determine which routes to create
+  const userRoles = user ? user.roles.map((role) => role.roleName) : [];
+
+  const isOperator = operatorRoutes.some((role) => userRoles.includes(role));
+  const isAdmin = adminRoutes.some((role) => userRoles.includes(role));
 
   return (
     <Router>
@@ -21,9 +40,25 @@ function App() {
         {user ? (
           <Route element={<Sidebar />}>
             <Route element={<PrivateRoute />}>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/clients" element={<Clients />} />
-              <Route path="*" element={<Navigate to="/dashboard" />} />
+              <Route path="/profile" element={<Profile />} />
+              {isAdmin ? (
+                <Route path="/home" element={<Dashboard />} />
+              ) : (
+                <Route
+                  path="/home"
+                  element={<Dummy data={"Portal Usuario"} />}
+                />
+              )}
+              {/*! Operator routes*/}
+              {isOperator && (
+                <Route path="/clients" element={<Clients />}>
+                  <Route path="" element={<ClientList />} />
+                  <Route path="create" element={<CreateClient />} />
+                  <Route path="view/:id" element={<ViewClient />} />
+                  <Route path="update/:id" element={<UpdateClient />} />
+                </Route>
+              )}
+              <Route path="*" element={<Navigate to="/home" />} />
             </Route>
           </Route>
         ) : (
