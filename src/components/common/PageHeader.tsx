0a4e49ca557294945bb82f8965fe9ce8b32d3a6a
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-07-07 17:22:14 -0600

    Update PageHeader.tsx
    Removed clutter

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -9,16 +9,31 @@ interface PageHeaderProps {
 
 function PageHeader({ title, children, size }: PageHeaderProps) {
   return (
-    <Stack direction={{ base: "column", sm: "column", md: "row" }}>
-      <Box flexGrow={1}>
-        <Heading fontSize={size}>{title}</Heading>
-      </Box>
-      <Stack
-        spacing={4}
-        direction={{ base: "column", md: "column", sm: "column", lg: "row" }}
-      >
-        {children}
+    <Stack>
+      <Stack direction={{ base: "column", md: "row" }}>
+        <Box flexGrow={1}>
+          <Heading fontSize={size}>{title}</Heading>
+        </Box>
+        <Stack
+          spacing={4}
+          direction={{
+            base: "column",
+            sm: "column",
+            md: "column",
+            lg: "column",
+            xl: "row",
+          }}
+        >
+          {children}
+        </Stack>
       </Stack>
+      {/*       <Stack pt={4}>
+        <Alert status="info">
+          <AlertIcon />
+          Asegúrate de ingresar la información de manera correcta. Respeta
+          mayusculas y minusculas, ademas de signos de puntuación.
+        </Alert>
+      </Stack> */}
     </Stack>
   );
 }
