8f49e4b905f5acd364e133b55bd7516df3452190
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-07-07 17:20:40 -0600

    Create location.ts
    Helper functions related to location

---
+++ src/helpers/location.ts
@@ -0,0 +1,65 @@
+/**
+ * Get nearby location coordinates within a specified distance range.
+ *
+ * @param {number} lat - The latitude of the center point.
+ * @param {number} lng - The longitude of the center point.
+ * @param {number} distance - The distance range in kilometers.
+ * @param {number} [numLocations=5] - The maximum number of nearby locations to return.
+ * @returns {Array<{ lat: number, lng: number }>} - An array of nearby location coordinates.
+ */
+export function getNearbyLocations(
+  lat: number,
+  lng: number,
+  distance: number,
+  numLocations = 5
+) {
+  // Constants for latitude and longitude conversions
+  const DEG_TO_RAD = Math.PI / 180;
+  const EARTH_RADIUS = 6371; // Earth's radius in kilometers
+
+  // Convert distance to radians
+  const distanceRad = distance / EARTH_RADIUS;
+
+  // Convert center point coordinates to radians
+  const centerLatRad = lat * DEG_TO_RAD;
+  const centerLngRad = lng * DEG_TO_RAD;
+
+  const locations = [];
+
+  // Generate random nearby locations
+  while (locations.length < numLocations) {
+    // Generate random bearing (in radians)
+    const bearingRad = Math.random() * 2 * Math.PI;
+
+    // Generate random distance (in radians)
+    const distanceRandRad = Math.random() * distanceRad;
+
+    // Calculate latitude of the nearby location
+    const nearbyLatRad = Math.asin(
+      Math.sin(centerLatRad) * Math.cos(distanceRandRad) +
+        Math.cos(centerLatRad) *
+          Math.sin(distanceRandRad) *
+          Math.cos(bearingRad)
+    );
+
+    // Calculate longitude of the nearby location
+    const nearbyLngRad =
+      centerLngRad +
+      Math.atan2(
+        Math.sin(bearingRad) *
+          Math.sin(distanceRandRad) *
+          Math.cos(centerLatRad),
+        Math.cos(distanceRandRad) -
+          Math.sin(centerLatRad) * Math.sin(nearbyLatRad)
+      );
+
+    // Convert nearby location coordinates to degrees
+    const nearbyLat = nearbyLatRad / DEG_TO_RAD;
+    const nearbyLng = nearbyLngRad / DEG_TO_RAD;
+
+    // Add the nearby location to the array
+    locations.push({ lat: nearbyLat, lng: nearbyLng });
+  }
+
+  return locations;
+}
