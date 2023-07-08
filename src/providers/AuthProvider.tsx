525e622884aeac1dece98851f8e117db320b8932
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-07-07 19:27:40 -0600

    Update AuthProvider.tsx
    Extracted constants and interfaces to be able to use the Vite.js + React + TypeScript template HMR for better DX and faster development

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,35 +1,7 @@
-import jwtDecode from "jwt-decode";
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { User } from "interfaces/User";
-
-interface AuthContextValue {
-  user: User | null;
-  setUser: React.Dispatch<React.SetStateAction<User | null>> | null;
-  login: (user: User) => void;
-  logout: () => void;
-}
-
-export const AuthContext = createContext<AuthContextValue>({
-  user: null,
-  setUser: null,
-  login: () => {},
-  logout: () => {},
-});
-
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
-
-export const isTokenExpired = (token: string) => {
-  const decodedToken = jwtDecode(token) as { exp: number };
-  return decodedToken.exp < Date.now() / 1000;
-};
+import { ReactNode, useEffect, useState } from "react";
+import { isTokenExpired } from "../helpers/encryption";
+import { User } from "../interfaces/User";
+import { AuthContext } from "../context/AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(() => {
