ba2a5f38a4764b661d81356e1dee0ffbe4a4f110
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-11 13:18:15 -0600

    Update ValidatableInputProps.ts
    Modified interface accordingly

--- src/interfaces/validation/ValidatableInputProps.ts
+++ src/interfaces/validation/ValidatableInputProps.ts
@@ -1,3 +1,4 @@
+import { ResponsiveValue, ThemingProps } from "@chakra-ui/react";
 import { FieldError, UseFormRegisterReturn } from "react-hook-form";
 
 export interface ValidatableInputProps {
@@ -7,9 +8,13 @@ export interface ValidatableInputProps {
   error?: FieldError;
   register?: UseFormRegisterReturn;
   required?: boolean;
+  readonly?: boolean;
   value?: string | number | readonly string[] | undefined;
   defaultValue?: string | number | readonly string[] | undefined;
   disabled?: boolean | undefined;
   helperText?: string | undefined;
   leftElement?: JSX.Element;
+  variant?:
+    | ResponsiveValue<"outline" | "unstyled" | "filled" | "flushed">
+    | undefined;
 }
