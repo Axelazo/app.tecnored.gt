434c707d85aece1941aa1a5a4b7c2dc68dd7e4d9
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-21 19:28:34 -0600

    feat: Added Clients Services rendering on the map
    Introduces the capability to render clients' services on a map. This enhancement enhances user experience by visually displaying service locations directly on the map interface.
    
    The addition is aimed at improving the accessibility and usability of service-related information, facilitating quicker decision-making and interaction with service data.

--- src/pages/monitoring/Monitoring.tsx
+++ src/pages/monitoring/Monitoring.tsx
@@ -1,84 +1,130 @@
 import {
-  Button,
   Card,
-  Flex,
   FormControl,
   FormLabel,
-  Heading,
   HStack,
-  Icon,
   Input,
   Select,
   Spinner,
   Stack,
-  Text,
 } from "@chakra-ui/react";
-import {
-  GoogleMap,
-  OverlayViewF,
-  MarkerF,
-  useLoadScript,
-  Rectangle,
-  RectangleF,
-} from "@react-google-maps/api";
-import { getNearbyLocations } from "../../helpers/location";
-import { useState } from "react";
-
-import { HiStatusOnline, HiStatusOffline } from "react-icons/hi";
-import { MdOutlineSignalCellularAlt, MdSpeed } from "react-icons/md";
-import { TbArrowsLeftRight } from "react-icons/tb";
-import { FaTicketAlt } from "react-icons/fa";
-import { BsRouterFill } from "react-icons/bs";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
+import useApiClient from "../../hooks/useApiClient";
+import { ApiResponse } from "../../interfaces/misc/ApiResponse";
+import { ClientServiceMarker } from "../../components/services/monitoring/ClientServiceMarker";
+import { ClientServiceOverlay } from "../../components/services/monitoring/ClientServiceOverlay";
+import { ServiceLocation } from "../../interfaces/tables/ServiceLocation";
 
 function Monitoring() {
-  //Check if the map is loaded, returns flag isLoaded
+  //Google maps
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCTLAIUGQcxfCfTIB3ax80mMMT_YuHRQLc",
-    libraries: ["geometry"],
+    //libraries: ["geometry"],
   });
-
-  //Center of the map
   const [center, setCenter] = useState({
     lat: 16.509673990914745,
     lng: -89.41893294734292,
   });
-  //Current zoom level
   const [zoom, setZoom] = useState(15);
-
-  //Current bounds
   const [bounds, setBounds] = useState<
     google.maps.LatLngBounds | google.maps.LatLngBoundsLiteral | undefined
   >(undefined);
 
-  //Current map instance
   const [mapInstance, setMapInstance] = useState<google.maps.Map | null>(null);
 
-  //Locations of the services on the map
-  const [locations, setLocations] = useState<
-    { lat: number; lng: number }[] | null
-  >(null);
+  // Services and Location
+  const [services, setServices] = useState<ServiceLocation[] | null>(null);
+  const [service, setService] = useState<ServiceLocation | null>(null);
+  const apiClient = useApiClient();
+
+  // Rendering logic
+  const [selectedMarker, setSelectedMarker] = useState<number | null>(null);
+  const handleMarkerClick = (id: number, service: ServiceLocation) => {
+    setSelectedMarker(id);
+    setService(service);
+  };
+
+  const handleMapClick = () => {
+    setSelectedMarker(null);
+  };
+
+  const mapOptions = {
+    mapId: "3e5e9ba702566ad8",
+    mapTypeControl: false, // hide Map | Satellite button
+    fullscreenControl: false, // hide Expand button
+    zoomControl: true, // hide + and - buttons
+    streetViewControl: false, // hide street view button
+  };
+
+  const mapContainerStyle: React.CSSProperties = {
+    boxSizing: "content-box",
+    minWidth: "100%",
+    height: "720px",
+    borderRadius: "15px",
+  };
 
   const handleMapLoad = (map: google.maps.Map) => {
     setMapInstance(map);
-    console.log(map);
-    const nearbyLocations = getNearbyLocations(
+    /*     const nearbyLocations = getNearbyLocations(
       16.509673990914745,
       -89.41893294734292,
       10,
       100
-    );
-
-    setLocations(nearbyLocations);
+    ); */
   };
 
   const handleBoundsChange = () => {
-    let bounds;
-    if (mapInstance) {
-      bounds = mapInstance.getBounds();
-    }
+    setTimeout(() => {
+      let bounds;
+      if (mapInstance) {
+        bounds = mapInstance.getBounds();
+      }
 
-    if (bounds) {
-      setBounds(bounds);
+      if (bounds) {
+        setBounds(bounds);
+      }
+    }, 200);
+    console.log(`Bounds changed!`);
+  };
+
+  useEffect(() => {
+    if (mapInstance && service) {
+      const timeoutId = setTimeout(() => {
+        mapInstance.panTo({
+          lat: parseFloat(service.latitude),
+          lng: parseFloat(service.longitude),
+        });
+      }, 500);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [service, mapInstance]);
+
+  useEffect(() => {
+    servicesResponse();
+  }, [bounds]);
+
+  const servicesResponse = async () => {
+    if (bounds instanceof google.maps.LatLngBounds) {
+      const northEast = bounds.getNorthEast();
+      const southWest = bounds.getSouthWest();
+
+      const north = northEast.lat();
+      const east = northEast.lng();
+      const south = southWest.lat();
+      const west = southWest.lng();
+
+      await apiClient
+        .get<ApiResponse<ServiceLocation[]>>(
+          `/services/area?south=${south}&west=${west}&north=${north}&east=${east}`
+        )
+        .then((response) => {
+          setServices(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   };
 
@@ -103,23 +149,44 @@ function Monitoring() {
           <GoogleMap
             onLoad={handleMapLoad}
             onIdle={handleBoundsChange}
-            onBoundsChanged={handleBoundsChange}
             center={center}
             zoom={zoom}
-            options={{
-              mapId: "3e5e9ba702566ad8",
-              mapTypeControl: false, // hide Map | Satellite button
-              fullscreenControl: false, // hide Expand button
-              zoomControl: true, // hide + and - buttons
-              streetViewControl: false, // hide street view button
-            }}
-            mapContainerStyle={{
-              boxSizing: "content-box",
-              minWidth: "100%",
-              height: "720px",
-              borderRadius: "15px",
-            }}
-          ></GoogleMap>
+            options={mapOptions}
+            mapContainerStyle={mapContainerStyle}
+            onClick={handleMapClick}
+          >
+            <>
+              {/*bounds ? <RectangleF bounds={bounds}></RectangleF> : "" */}
+              {services
+                ? services.map((service) => {
+                    return (
+                      <>
+                        <ClientServiceMarker
+                          setServiceId={() => {
+                            handleMarkerClick(service.id, service);
+                          }}
+                          id={service.id}
+                          service={service}
+                          position={{
+                            lat: parseFloat(service.latitude),
+                            lng: parseFloat(service.longitude),
+                          }}
+                        />
+                      </>
+                    );
+                  })
+                : ""}
+              {selectedMarker && service && services && (
+                <ClientServiceOverlay
+                  id={selectedMarker}
+                  position={{
+                    lat: parseFloat(service.latitude),
+                    lng: parseFloat(service.longitude),
+                  }}
+                />
+              )}
+            </>
+          </GoogleMap>
         ) : (
           <Spinner />
         )}
