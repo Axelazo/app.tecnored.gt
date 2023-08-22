57ade6118e52cb1539c259277365316c6be6717d
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-21 19:26:34 -0600

    feat: add functions for reducing LatLngBounds
    Added a set of functions to manipulate LatLngBounds objects in various ways:
    
    reduceBoundsByDistance: This function reduces the given LatLngBounds by a specified distance in meters from the center. It calculates latitude and longitude differences based on Earth's radius and shifts the corners accordingly.
    
    reduceBoundsByDistanceAlternative: An alternative approach to reducing bounds by distance. It approximates degrees of latitude and longitude per meter to shift the corners toward the center.
    
    reduceBoundsByPercentage: This function similarly reduces the LatLngBounds but uses a percentage value of the original dimensions for reduction.
    
    These functions are intended to facilitate manipulation of LatLngBounds for various map-related operations.

--- src/helpers/location.ts
+++ src/helpers/location.ts
@@ -87,3 +87,108 @@ export function roundAndSplitCoordinates(coordinates: string, decimals = 6) {
 
   return `${roundedLatitude}, ${roundedLongitude}`;
 }
+/* 
+
+ * Reduces the given LatLngBounds by a specified distance in meters from the center.
+ *
+ * @param {google.maps.LatLngBounds | google.maps.LatLngBoundsLiteral} bounds - The original bounds to be reduced.
+ * @param {number} distanceMeters - The distance in meters by which to reduce the bounds.
+ * @returns {google.maps.LatLngBounds} - The reduced bounds.
+ 
+export function reduceBoundsByDistance(
+  bounds: google.maps.LatLngBounds | google.maps.LatLngBoundsLiteral,
+  distanceMeters: number
+): google.maps.LatLngBounds {
+  let center: google.maps.LatLng;
+
+  if (bounds instanceof google.maps.LatLngBounds) {
+    center = bounds.getCenter();
+  } else {
+    // Handle the case of google.maps.LatLngBoundsLiteral
+    const centerLat = (bounds.north + bounds.south) / 2;
+    const centerLng = (bounds.east + bounds.west) / 2;
+    center = new google.maps.LatLng(centerLat, centerLng);
+  }
+
+  // Convert the distance from meters to latitude/longitude degrees
+  const latLngDistance = distanceMeters / 111111; // Rough approximation
+
+  // Calculate the new bounds by adjusting the center
+  const newSouth = center.lat() - latLngDistance;
+  const newWest = center.lng() - latLngDistance;
+  const newNorth = center.lat() + latLngDistance;
+  const newEast = center.lng() + latLngDistance;
+
+  // Create and return the new bounds
+  const newBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(newSouth, newWest),
+    new google.maps.LatLng(newNorth, newEast)
+  );
+
+  console.log(JSON.stringify(newBounds.toJSON()));
+  return newBounds;
+}
+ */
+
+export function reduceBoundsByDistance(
+  originalBounds: google.maps.LatLngBounds,
+  distanceMeters: number
+): google.maps.LatLngBounds {
+  const SW = originalBounds.getSouthWest();
+  const NE = originalBounds.getNorthEast();
+
+  const latDiff =
+    (NE.lat() - SW.lat()) * (distanceMeters / (2 * Math.PI * 6371000)); // 6371000 is Earth's radius in meters
+  const lngDiff =
+    (NE.lng() - SW.lng()) *
+    ((distanceMeters / (2 * Math.PI * 6371000)) *
+      Math.cos(((NE.lat() + SW.lat()) / 2) * (Math.PI / 180)));
+
+  const newSW = new google.maps.LatLng(SW.lat() + latDiff, SW.lng() + lngDiff);
+  const newNE = new google.maps.LatLng(NE.lat() - latDiff, NE.lng() - lngDiff);
+
+  const newBounds = new google.maps.LatLngBounds(newSW, newNE);
+  return newBounds;
+}
+
+export function reduceBoundsByDistanceAlternative(
+  originalBounds: google.maps.LatLngBounds,
+  distanceMeters: number
+): google.maps.LatLngBounds {
+  const SW = originalBounds.getSouthWest();
+  const NE = originalBounds.getNorthEast();
+  const center = originalBounds.getCenter();
+
+  // Convert distance to degrees of latitude and longitude
+  const latDiff = distanceMeters / 111320; // Approximate degrees of latitude per meter
+  const lngDiff =
+    distanceMeters / (111320 * Math.cos((center.lat() * Math.PI) / 180)); // Approximate degrees of longitude per meter
+
+  // Shift the corners toward the center by the calculated differences
+  const newSW = new google.maps.LatLng(SW.lat() + latDiff, SW.lng() + lngDiff);
+  const newNE = new google.maps.LatLng(NE.lat() - latDiff, NE.lng() - lngDiff);
+
+  const newBounds = new google.maps.LatLngBounds(newSW, newNE);
+  return newBounds;
+}
+
+export function reduceBoundsByPercentage(
+  originalBounds: google.maps.LatLngBounds,
+  distanceMeters: number
+): google.maps.LatLngBounds {
+  const SW = originalBounds.getSouthWest();
+  const NE = originalBounds.getNorthEast();
+  const center = originalBounds.getCenter();
+
+  // Convert distance to degrees of latitude and longitude
+  const latDiff = distanceMeters / 111320; // Approximate degrees of latitude per meter
+  const lngDiff =
+    distanceMeters / (111320 * Math.cos((center.lat() * Math.PI) / 180)); // Approximate degrees of longitude per meter
+
+  // Shift the corners toward the center by the calculated differences
+  const newSW = new google.maps.LatLng(SW.lat() + latDiff, SW.lng() + lngDiff);
+  const newNE = new google.maps.LatLng(NE.lat() - latDiff, NE.lng() - lngDiff);
+
+  const newBounds = new google.maps.LatLngBounds(newSW, newNE);
+  return newBounds;
+}
