1d761284ac51c6640f0ce8e7031871d90ccc6028
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-23 18:39:52 -0600

    feat: added Interface for Tickets form

---
+++ src/formValues/TicketFormValues.ts
@@ -0,0 +1,11 @@
+export interface TicketFormValues {
+  clientId: number;
+  serviceId: number;
+  reasonId: number;
+  employeeId: number;
+  customReason?: string;
+  estimatedStart: Date;
+  estimatedFinish: Date;
+  comment?: string;
+  priority: number;
+}
