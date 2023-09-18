9b13bf964319b7d9b262ac08be1a3895ae81ee71
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-09-18 16:49:32 -0600

    chore: removed React.StricMode temporarily

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import AuthProvider from "./providers/AuthProvider.tsx";
 import AppWrapper from "./AppWrapper.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <AppWrapper />
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <AppWrapper />
+  </AuthProvider>
 );
